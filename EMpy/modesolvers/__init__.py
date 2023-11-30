78733cb1ae0386eceffa02c537fd46a64cd8edb4
Zihang <stevezhwang@gmail.com>
2023-11-29 17:39:41 -0800

    init

---
+++ EMpy/modesolvers/__init__.py
@@ -0,0 +1,13 @@
+"""Collection of different algorithms to find modes of optical waveguides.
+
+Modesolvers:
+
+    1. FD.
+    2. FMM.
+
+
+"""
+__all__ = ["FD", "FMM", "geometries"]
+__author__ = "Lorenzo Bolla"
+
+from . import FD, FMM, geometries
